8b3d3dbd4958f8f6e1a9723c1a1d7f9abf123181
Markus Kahl <machisuji@gmail.com>
2019-07-30 00:09:43 +0100

    amended migration moving estimated hours to derived ~

--- db/migrate/20190722082648_add_derived_estimated_hours_to_work_packages.rb
+++ db/migrate/20190722082648_add_derived_estimated_hours_to_work_packages.rb
@@ -1,5 +1,33 @@
 class AddDerivedEstimatedHoursToWorkPackages < ActiveRecord::Migration[5.2]
+  class WorkPackageWithRelations < ActiveRecord::Base
+    self.table_name = WorkPackage.table_name
+
+    scope :with_children, ->(*args) do
+      rel = Relation.table_name
+      wp = WorkPackage.table_name
+
+      query = "EXISTS (SELECT 1 FROM #{rel} WHERE #{rel}.from_id = #{wp}.id AND #{rel}.hierarchy > 0 LIMIT 1)"
+
+      where(query)
+    end
+  end
+
   def change
     add_column :work_packages, :derived_estimated_hours, :float
+
+    reversible do |change|
+      change.up do
+        # Before this migration all work packages who have children had their
+        # estimated hours set based on their children through the UpdateAncestorsService.
+        #
+        # We move this value to the derived_estimated_hours column and clear
+        # the estimated_hours column. In the future users can estimte the time
+        # for parent work pacages separately there while the UpdateAncestorsService
+        # only touches the derived_estimated_hours column.
+        WorkPackageWithRelations
+          .with_children
+          .update_all("derived_estimated_hours = estimated_hours, estimated_hours = NULL")
+      end
+    end
   end
 end
